3b3576a937328ff8af0ee63b477e3e3deb00ec48
John Kelly <john.kelly@digital.hmrc.gov.uk>
2020-08-19 12:36:23 +0100

    CTDA-583: Update Test Support API to support IE058

--- test/controllers/actions/ValidateArrivalMessageTypeActionSpec.scala
+++ test/controllers/actions/ValidateArrivalMessageTypeActionSpec.scala
@@ -139,6 +139,28 @@ class ValidateArrivalMessageTypeActionSpec
       status(result) mustEqual OK
     }
 
+    "must execute the block when passed in a valid IE058 TestMessage" in {
+      val validateMessageType = app.injector.instanceOf[ValidateArrivalMessageTypeAction]
+      val cc                  = app.injector.instanceOf[ControllerComponents]
+
+      val controller = new Harness(validateMessageType, cc)
+
+      val exampleRequest: JsValue = Json.parse(
+        """{
+          |     "message": {
+          |         "messageType": "IE058"
+          |     }
+          | }""".stripMargin
+      )
+
+      val req: FakeRequest[JsValue] =
+        FakeRequest(method = "", uri = "", headers = FakeHeaders(Seq(HeaderNames.CONTENT_TYPE -> "application/json")), exampleRequest)
+
+      val result = controller.post()(req)
+
+      status(result) mustEqual OK
+    }
+
     "must generate correct IE008 message in executed block" in {
       val validateMessageType = app.injector.instanceOf[ValidateArrivalMessageTypeAction]
       val cc                  = app.injector.instanceOf[ControllerComponents]
@@ -212,6 +234,31 @@ class ValidateArrivalMessageTypeActionSpec
 
       status(result) mustEqual OK
       numberOfNodes(contentAsXml(result)) mustEqual numberOfNodes(ie043)
+    }
+
+    "must generate correct IE058 message in executed block" in {
+      val validateMessageType = app.injector.instanceOf[ValidateArrivalMessageTypeAction]
+      val cc                  = app.injector.instanceOf[ControllerComponents]
+
+      val ie058: NodeSeq = Messages.Arrival.SupportedMessageTypes(TestMessage(MessageType.UnloadingRemarksRejection.code))()
+
+      val controller = new Harness(validateMessageType, cc)
+
+      val exampleRequest: JsValue = Json.parse(
+        """{
+          |     "message": {
+          |         "messageType": "IE058"
+          |     }
+          | }""".stripMargin
+      )
+
+      val req: FakeRequest[JsValue] =
+        FakeRequest(method = "", uri = "", headers = FakeHeaders(Seq(HeaderNames.CONTENT_TYPE -> "application/json")), exampleRequest)
+
+      val result = controller.post()(req)
+
+      status(result) mustEqual OK
+      numberOfNodes(contentAsXml(result)) mustEqual numberOfNodes(ie058)
     }
   }
 }
