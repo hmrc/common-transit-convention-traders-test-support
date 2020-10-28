70b2dd6048fd84f334d58d4a78114c3f0f97ce21
crystalmark <tim@crystalmark.co.uk>
2020-10-28 15:37:05 +0100

    CTDA-876 changed CC051A to B

--- app/models/MessageType.scala
+++ app/models/MessageType.scala
@@ -28,7 +28,7 @@ sealed trait MessageType extends IeMetadata {
 object MessageType extends Enumerable.Implicits {
 
   case object PositiveAcknowledgement     extends IeMetadata("IE928", "CC928A") with MessageType
-  case object NoReleaseForTransit         extends IeMetadata("IE051", "CC051A") with MessageType
+  case object NoReleaseForTransit         extends IeMetadata("IE051", "CC051B") with MessageType
   case object ReleaseForTransit           extends IeMetadata("IE029", "CC029A") with MessageType
   case object ControlDecisionNotification extends IeMetadata("IE060", "CC060A") with MessageType
   case object MrnAllocated                extends IeMetadata("IE028", "CC028A") with MessageType
--- app/utils/Messages.scala
+++ app/utils/Messages.scala
@@ -18,6 +18,7 @@ package utils
 
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 import models.MessageType.ArrivalRejection
@@ -188,7 +189,7 @@ object Messages {
 
     def generateIE051Message(): NodeSeq = {
       val xml =
-        <CC051A>
+        <CC051B>
           <SynIdeMES1>{Strings.alpha(4)}</SynIdeMES1>
           <SynVerNumMES2>{Strings.numeric(1)}</SynVerNumMES2>
           <MesSenMES3>{Strings.alphanumeric(1, 35)}</MesSenMES3>
@@ -196,44 +197,90 @@ object Messages {
           <DatOfPreMES9>{LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))}</DatOfPreMES9>
           <TimOfPreMES10>{LocalTime.now().format(DateTimeFormatter.ofPattern("HHmm"))}</TimOfPreMES10>
           <IntConRefMES11>{Strings.alphanumeric(1, 14)}</IntConRefMES11>
+          <AppRefMES14>NCTS</AppRefMES14>
+          <TesIndMES18>0</TesIndMES18>
           <MesIdeMES19>{Strings.alphanumeric(1, 14)}</MesIdeMES19>
-          <MesTypMES20>{Strings.alphanumeric(1, 6)}</MesTypMES20>
+          <MesTypMES20>GB051B</MesTypMES20>
           <HEAHEA>
             <RefNumHEA4>{Strings.alphanumeric(1, 22)}</RefNumHEA4>
             <DocNumHEA5>{Strings.alphanumeric(1, 21)}</DocNumHEA5>
-            <TypOfDecHEA24>{Strings.alphanumeric(1, 9)}</TypOfDecHEA24>
-            <ConIndHEA96>{Strings.numeric(1)}</ConIndHEA96>
-            <NCTSAccDocHEA601LNG>{Strings.alpha(2)}</NCTSAccDocHEA601LNG>
-            <TotNumOfIteHEA305>{Strings.numeric(5)}</TotNumOfIteHEA305>
-            <TotGroMasHEA307>{Strings.decimalMax12()}</TotGroMasHEA307>
-            <DecDatHEA383>{Strings.numeric8()}</DecDatHEA383>
-            <DecPlaHEA394>{Strings.alphanumeric(1, 35)}</DecPlaHEA394>
+            <TypOfDecHEA24>T1</TypOfDecHEA24>
+            <CouOfDesCodHEA30>IT</CouOfDesCodHEA30>
+            <CouOfDisCodHEA55>GB</CouOfDisCodHEA55>
+            <IdeOfMeaOfTraAtDHEA78>NC15 REG</IdeOfMeaOfTraAtDHEA78>
+            <ConIndHEA96>0</ConIndHEA96>
+            <DiaLanIndAtDepHEA254>EN</DiaLanIndAtDepHEA254>
+            <NCTSAccDocHEA601LNG>EN</NCTSAccDocHEA601LNG>
+            <TotNumOfIteHEA305>1</TotNumOfIteHEA305>
+            <TotNumOfPacHEA306>10</TotNumOfPacHEA306>
+            <TotGroMasHEA307>1000</TotGroMasHEA307>
+            <DecDatHEA383>{LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))}</DecDatHEA383>
+            <DecPlaHEA394>Dover</DecPlaHEA394>
+            <NoRelMotHEA272>Test</NoRelMotHEA272>
           </HEAHEA>
-          <TRAPRIPC1/>
+          <TRAPRIPC1>
+            <NamPC17>NCTS UK TEST LAB HMCE</NamPC17>
+            <StrAndNumPC122>11TH FLOOR, ALEX HOUSE, VICTORIA AV</StrAndNumPC122>
+            <PosCodPC123>SS99 1AA</PosCodPC123>
+            <CitPC124>SOUTHEND-ON-SEA, ESSEX</CitPC124>
+            <CouPC125>GB</CouPC125>
+            <TINPC159>GB954131533000</TINPC159>
+          </TRAPRIPC1>
+          <TRACONCO1>
+            <NamCO17>NCTS UK TEST LAB HMCE</NamCO17>
+            <StrAndNumCO122>11TH FLOOR, ALEX HOUSE, VICTORIA AV</StrAndNumCO122>
+            <PosCodCO123>SS99 1AA</PosCodCO123>
+            <CitCO124>SOUTHEND-ON-SEA, ESSEX</CitCO124>
+            <CouCO125>GB</CouCO125>
+            <TINCO159>GB954131533000</TINCO159>
+          </TRACONCO1>
+          <TRACONCE1>
+            <NamCE17>NCTS UK TEST LAB HMCE</NamCE17>
+            <StrAndNumCE122>ITALIAN OFFICE</StrAndNumCE122>
+            <PosCodCE123>IT99 1IT</PosCodCE123>
+            <CitCE124>MILAN</CitCE124>
+            <CouCE125>IT</CouCE125>
+            <TINCE159>IT11ITALIANC11</TINCE159>
+          </TRACONCE1>
           <CUSOFFDEPEPT>
-            <RefNumEPT1>{Strings.alphanumeric(8)}</RefNumEPT1>
+            <RefNumEPT1>GB000060</RefNumEPT1>
           </CUSOFFDEPEPT>
+          <CUSOFFTRARNS>
+            <RefNumRNS1>FR001260</RefNumRNS1>
+            <ArrTimTRACUS085>{LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"))}</ArrTimTRACUS085>
+          </CUSOFFTRARNS>
           <CUSOFFDESEST>
-            <RefNumEST1>{Strings.alphanumeric(8)}</RefNumEST1>
+            <RefNumEST1>IT018105</RefNumEST1>
           </CUSOFFDESEST>
           <CONRESERS>
-            <ConDatERS14>{Strings.numeric8()}</ConDatERS14>
-            <ConResCodERS16>{Strings.alphanumeric(2)}</ConResCodERS16>
+            <ConDatERS14>{LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))}</ConDatERS14>
+            <ConResCodERS16>B1</ConResCodERS16>
           </CONRESERS>
-          <GUAGUA>
-            <GuaTypGUA1>{Strings.alphanumeric(1)}</GuaTypGUA1>
+          <RESOFCON534>
+            <DesTOC2>See Header for details</DesTOC2>
+            <ConInd424>OT</ConInd424>
+          </RESOFCON534>
+          <GUAGUA><GuaTypGUA1>1</GuaTypGUA1>
+            <GUAREFREF>
+              <GuaRefNumGRNREF1>{Strings.alphanumeric(1, 17)}</GuaRefNumGRNREF1>
+              <AccCodREF6>AC01</AccCodREF6>
+              <VALLIMECVLE><NotValForECVLE1>0</NotValForECVLE1>
+              </VALLIMECVLE>
+            </GUAREFREF>
           </GUAGUA>
           <GOOITEGDS>
-            <IteNumGDS7>{Strings.numeric(1,5)}</IteNumGDS7>
-            <GooDesGDS23>{Strings.alphanumeric(1, 280)}</GooDesGDS23>
-            <RESOFCONROC>
-              <ConIndROC1>{Strings.alphanumeric(2)}</ConIndROC1>
+            <IteNumGDS7>1</IteNumGDS7>
+            <GooDesGDS23>Daffodils</GooDesGDS23>
+            <GroMasGDS46>1000</GroMasGDS46>
+            <NetMasGDS48>950</NetMasGDS48>
+            <RESOFCONROC><ConIndROC1>OR</ConIndROC1>
             </RESOFCONROC>
-            <PACGS2>
-              <KinOfPacGS23>{Strings.alphanumeric(1, 3)}</KinOfPacGS23>
+            <PACGS2><MarNumOfPacGS21>AB234</MarNumOfPacGS21>
+              <KinOfPacGS23>BX</KinOfPacGS23>
+              <NumOfPacGS24>10</NumOfPacGS24>
             </PACGS2>
           </GOOITEGDS>
-        </CC051A>
+        </CC051B>
 
       xml
     }
