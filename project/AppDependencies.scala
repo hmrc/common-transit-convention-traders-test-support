91fc6ee45134ff7536b5a3941f3b823d1141f748
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2022-01-17 09:15:43 +0000

    CTCTRADERS-2890: Dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,26 +3,24 @@ import sbt._
 
 object AppDependencies {
 
-  private val catsVersion = "2.6.1"
-
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"       % "5.16.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"       % "5.19.0",
     "com.typesafe.play" %% "play-iteratees"                  % "2.6.1",
     "com.typesafe.play" %% "play-iteratees-reactive-streams" % "2.6.1",
     "org.scalacheck"    %% "scalacheck"                      % "1.15.4",
-    "org.typelevel"     %% "cats-core"                       % catsVersion
+    "org.typelevel"     %% "cats-core"                       % "2.7.0"
   )
 
   val test = Seq(
-    "org.mockito"            % "mockito-core"          % "3.3.3",
+    "org.mockito"            % "mockito-core"          % "4.2.0",
     "org.scalatest"          %% "scalatest"            % "3.2.10",
     "com.typesafe.play"      %% "play-test"            % current,
     "org.scalatestplus.play" %% "scalatestplus-play"   % "4.0.3",
     "org.scalatestplus"      %% "mockito-3-2"          % "3.1.2.0",
     "org.scalacheck"         %% "scalacheck"           % "1.15.4",
     "com.github.tomakehurst" % "wiremock-standalone"   % "2.27.2",
-    "org.typelevel"          %% "cats-laws"            % catsVersion,
-    "org.typelevel"          %% "discipline-core"      % "1.1.5",
+    "org.typelevel"          %% "cats-laws"            % "2.7.0",
+    "org.typelevel"          %% "discipline-core"      % "1.4.0",
     "org.typelevel"          %% "discipline-scalatest" % "2.1.5",
     "com.vladsch.flexmark"   % "flexmark-all"          % "0.62.2"
   ).map(_ % "test, it")
