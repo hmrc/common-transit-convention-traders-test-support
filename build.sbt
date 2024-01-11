113d53c606ce81eb73f7400e40a545c989cc57b0
Sandeep Kumar Soni <112713042+sonisn1988@users.noreply.github.com>
2024-01-11 14:15:21 +0000

    common-transit-convention-traders-test-support (Play 3.0 Migration)

--- build.sbt
+++ build.sbt
@@ -37,7 +37,6 @@ lazy val it = project
   .enablePlugins(PlayScala)
   .dependsOn(microservice % "test->test") // the "test->test" allows reusing test code and test dependencies
   .settings(DefaultBuildSettings.itSettings())
-  .settings(libraryDependencies ++= AppDependencies.test)
 
 lazy val scoverageSettings =
   Seq(
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,17 +7,18 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "org.typelevel"     %% "cats-core"                 % "2.9.0",
+    "org.typelevel"     %% "cats-core"                 % "2.10.0",
     "io.chrisdavenport" %% "cats-scalacheck"           % "0.3.2"
   )
 
   val test = Seq(
-    "org.mockito"        % "mockito-core"           % "4.2.0",
+    "org.mockito"        % "mockito-core"           % "5.8.0",
     "org.scalatestplus" %% "mockito-3-2"            % "3.1.2.0",
     "org.scalacheck"    %% "scalacheck"             % scalaCheckVersion,
-    "org.typelevel"     %% "cats-laws"              % "2.7.0",
-    "org.typelevel"     %% "discipline-core"        % "1.4.0",
-    "org.typelevel"     %% "discipline-scalatest"   % "2.1.5",
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion
+    "org.typelevel"     %% "cats-laws"              % "2.10.0",
+    "org.typelevel"     %% "discipline-core"        % "1.5.1",
+    "org.typelevel"     %% "discipline-scalatest"   % "2.2.0",
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
+    "io.chrisdavenport" %% "cats-scalacheck"        % "0.3.2"
   ).map(_ % Test)
 }
