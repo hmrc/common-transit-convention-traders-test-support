aa2fbbb6faa14b80568acbc175023392c0b5cec7
Arei Mohammed <arei.mohammed@digital.hmrc.gov.uk>
2025-08-27 12:12:39 +0100

    bootstrap upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion  = "9.11.0"
+  private val bootstrapVersion  = "10.1.0"
   private val scalaCheckVersion = "1.18.1"
 
   val compile: Seq[ModuleID] = Seq(
